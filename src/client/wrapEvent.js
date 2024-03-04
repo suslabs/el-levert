5ccf8fc688c5d6d5bce2ebb703fba02b8fb64565
Alex31TheDev <alexdraghia45@gmail.com>
2024-03-04 22:29:49 +0200

    Fix wrap event

--- src/client/wrapEvent.js
+++ src/client/wrapEvent.js
@@ -1,16 +1,43 @@
+function tagTester(name) {
+    const tag = "[object " + name + "]";
+
+    return obj => toString.call(obj) === tag;
+}
+
+const isFunction = tagTester("Function");
+
+function isPromise(obj) {
+    if (typeof obj !== "object") {
+        return false;
+    }
+
+    return isFunction(obj.then) && isFunction(obj.catch);
+}
+
+const wrapPromise = (logger, promise) =>
+    new Promise((resolve, reject) => {
+        promise.then(resolve).catch(err => {
+            logger.error(err);
+            resolve();
+        });
+    });
+
 const wrapEvent = (logger, func) =>
     function (...args) {
+        let out;
+
         try {
-            const out = func(...args);
-
-            if (typeof out === "object" && typeof out.then === "function") {
-                out.catch(err => logger?.error(err));
-            }
-
-            return out;
+            out = func(...args);
         } catch (err) {
-            logger?.error(err);
+            logger.error(err);
+            return;
         }
+
+        if (isPromise(out)) {
+            out = wrapPromise(logger, out);
+        }
+
+        return out;
     };
 
 export default wrapEvent;
