cb6008e1e664e9956e3622c062bc71f2d7083d6b
Alex31TheDev <alexdraghia45@gmail.com>
2024-03-05 17:34:14 +0200

    Make the thing into an embed

--- src/commands/perm/list.js
+++ src/commands/perm/list.js
@@ -1,4 +1,20 @@
+import { EmbedBuilder } from "discord.js";
+
 import { getClient } from "../../LevertClient.js";
+
+function formatGroups(groups) {
+    const format = groups
+        .map(
+            (x, i) =>
+                `${i + 1}. ${x.name} - Level ${x.level} - Users:\n` +
+                (x.users.length > 0
+                    ? x.users.map((y, j) => `    ${j + 1}. \`${y.username}\` (${y.id})`).join("\n")
+                    : "    none")
+        )
+        .join("\n");
+
+    return format;
+}
 
 export default {
     name: "list",
@@ -11,16 +27,14 @@ export default {
             return ":information_source: No permissions are registered.";
         }
 
-        const format = groups
-            .map(
-                (x, i) =>
-                    `${i + 1}. ${x.name} - Level ${x.level} - Users:\n` +
-                    (x.users.length > 0
-                        ? x.users.map((y, j) => `    ${j + 1}. \`${y.username}\` (${y.id})`).join("\n")
-                        : "    none")
-            )
-            .join("\n");
+        const format = formatGroups(groups),
+            embed = new EmbedBuilder().setDescription(format);
 
-        return `:information_source: Registered permissions:\n${format}`;
+        const out = {
+            content: ":information_source: Registered permissions:",
+            embeds: [embed]
+        };
+
+        return out;
     }
 };
