be2d52a3c6fe4778c7618d18f37eca9ec553fd94
Alex31TheDev <alexdraghia45@gmail.com>
2024-03-07 22:19:50 +0200

    Refactor help

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -2,7 +2,7 @@ import { getClient } from "../../LevertClient.js";
 
 export default {
     name: "help",
-    handler: (_, _, perm) => {
+    handler: (_1, _2, perm) => {
         const help = getClient().commandManager.getHelp(perm);
 
         return `:information_source: Available commands are: ${help}
