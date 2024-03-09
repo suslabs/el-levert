f38bf8c377a17ac3e0c99aad4acc69d62ee3f586
Alex31TheDev <alexdraghia45@gmail.com>
2024-03-09 18:52:23 +0200

    the

---
+++ src/vm/judge0/ErrorCodes.js
@@ -0,0 +1,18 @@
+const ErrorCodes = {
+    1: "In Queue",
+    2: "Processing",
+    3: "Accepted",
+    4: "Wrong Answer",
+    5: "Time Limit Exceeded",
+    6: "Compilation Error",
+    7: "Runtime Error (SIGSEGV)",
+    8: "Runtime Error (SIGXFSZ)",
+    9: "Runtime Error (SIGFPE)",
+    10: "Runtime Error (SIGABRT)",
+    11: "Runtime Error (NZEC)",
+    12: "Runtime Error (Other)",
+    13: "Internal Error",
+    14: "Exec Format Error"
+};
+
+export default ErrorCodes;
