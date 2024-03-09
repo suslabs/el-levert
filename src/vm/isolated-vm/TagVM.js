eb009ad47d3c09101fa8d38698775a8a69d374ee
Alex31TheDev <alexdraghia45@gmail.com>
2024-03-09 21:16:51 +0200

    Move functions

--- src/vm/isolated-vm/TagVM.js
+++ src/vm/isolated-vm/TagVM.js
@@ -5,51 +5,6 @@ import VMErrors from "./VMErrors.js";
 
 import Util from "../../util/Util.js";
 
-function processReply(msg) {
-    const client = getClient();
-    let out = JSON.parse(msg);
-
-    if (typeof out.content !== "undefined") {
-        const split = out.content.split("\n");
-
-        if (out.content.length > client.config.outCharLimit || split.length > client.config.outNewlineLimit) {
-            return Util.getFileAttach(out.content);
-        }
-    }
-
-    if (typeof out.file !== "undefined") {
-        if (typeof out.file.data === "object") {
-            out.file.data = Object.values(out.file.data);
-        }
-
-        out = {
-            ...out,
-            ...Util.getFileAttach(out.file.data, out.file.name)
-        };
-    }
-
-    delete out.file;
-    return out;
-}
-
-function handleError(err) {
-    switch (err.name) {
-        case "ExitError":
-            return err.exitData;
-        case "ManevraError":
-            return processReply(err.message);
-    }
-
-    switch (err.message) {
-        case VMErrors.timeout:
-            return ":no_entry_sign: " + err.message;
-        case VMErrors.memLimit:
-            return ":no_entry_sign: Memory limit reached.";
-        default:
-            throw err;
-    }
-}
-
 class TagVM {
     constructor() {
         this.memLimit = getClient().config.memLimit;
@@ -57,6 +12,9 @@ class TagVM {
 
         this.enableInspector = getClient().config.enableInspector;
         this.inspectorPort = getClient().config.inspectorPort;
+
+        this.outCharLimit = getClient().config.outCharLimit;
+        this.outNewlineLimit = getClient().config.outNewlineLimit;
     }
 
     async runScript(code, msg, args) {
@@ -77,12 +35,56 @@ class TagVM {
                 res = res.toString();
             }
         } catch (err) {
-            res = handleError(err);
+            res = this.handleError(err);
         } finally {
             context.disposeIsolate();
         }
 
         return res;
+    }
+
+    handleError(err) {
+        switch (err.name) {
+            case "ExitError":
+                return err.exitData;
+            case "ManevraError":
+                return this.processReply(err.message);
+        }
+
+        switch (err.message) {
+            case VMErrors.timeout:
+                return ":no_entry_sign: " + err.message;
+            case VMErrors.memLimit:
+                return ":no_entry_sign: Memory limit reached.";
+            default:
+                throw err;
+        }
+    }
+
+    processReply(msg) {
+        let out = JSON.parse(msg);
+
+        if (typeof out.content !== "undefined") {
+            const split = out.content.split("\n");
+
+            if (out.content.length > this.outCharLimit || split.length > this.outNewlineLimit) {
+                return Util.getFileAttach(out.content);
+            }
+        }
+
+        if (typeof out.file !== "undefined") {
+            if (typeof out.file.data === "object") {
+                out.file.data = Object.values(out.file.data);
+            }
+
+            out = {
+                ...out,
+                ...Util.getFileAttach(out.file.data, out.file.name)
+            };
+        }
+
+        delete out.file;
+        return out;
     }
 }
 
