e0105c3a3e8d5bea1a3821ff27ae24653f3f7f46
Alex31TheDev <alexdraghia45@gmail.com>
2024-03-24 16:24:45 +0200

    Fix error skill issue 2

--- src/errors/DatabaseError.js
+++ src/errors/DatabaseError.js
@@ -1,9 +1,22 @@
 class DatabaseError extends Error {
-    constructor(message = "", ...args) {
+    constructor(obj, ...args) {
+        let message = "";
+
+        if (typeof obj === "object") {
+            message = obj.message;
+        } else {
+            message = obj;
+        }
+
         super(message, ...args);
 
         this.name = "DatabaseError";
         this.message = message;
+
+        if (typeof obj === "object") {
+            this.code = obj.code;
+            this.errno = obj.errno;
+        }
     }
 }
 
