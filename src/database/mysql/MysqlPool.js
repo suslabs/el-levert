5112c30fe6d78f82a1e1eecb42d00c4e5c10fa83
Alex31TheDev <alexdraghia45@gmail.com>
2024-04-02 21:35:56 +0300

    Wrapper 2

--- src/database/mysql/MysqlPool.js
+++ src/database/mysql/MysqlPool.js
@@ -1,7 +1,10 @@
 import mysql from "mysql";
 import EventEmitter from "events";
 
+import DatabaseError from "../../errors/DatabaseError.js";
+
 import PoolEvents from "./PoolEvents.js";
+import MysqlConnection from "./MysqlConnection.js";
 
 class MysqlPool extends EventEmitter {
     constructor(config) {
@@ -27,7 +30,114 @@ class MysqlPool extends EventEmitter {
         }
     }
 
-    getConnection() {}
+    getConnection() {
+        return new Promise((resolve, reject) => {
+            this.pool.getConnection((err, connection) => {
+                if (err) {
+                    if (this.throwErrors) {
+                        reject(new DatabaseError(err));
+                    } else {
+                        resolve();
+                    }
+                }
+
+                resolve(new MysqlConnection(connection, this.throwErrors));
+            });
+        });
+    }
+
+    acquireConnection(connection) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.pool.acquireConnection(connection.con, (err, connection) => {
+                    if (err) {
+                        if (this.throwErrors) {
+                            reject(new DatabaseError(err));
+                        } else {
+                            resolve();
+                        }
+                    }
+
+                    resolve(new MysqlConnection(connection, this.throwErrors));
+                });
+            } catch (err) {
+                if (this.throwErrors) {
+                    reject(new DatabaseError(err));
+                } else {
+                    resolve();
+                }
+            }
+        });
+    }
+
+    releaseConnection(connection) {
+        return new Promise((resolve, reject) => {
+            try {
+                this.pool.releaseConnection(connection.con, (err, connection) => {
+                    if (err) {
+                        if (this.throwErrors) {
+                            reject(new DatabaseError(err));
+                        } else {
+                            resolve();
+                        }
+                    }
+
+                    resolve();
+                });
+            } catch (err) {
+                if (this.throwErrors) {
+                    reject(new DatabaseError(err));
+                } else {
+                    resolve();
+                }
+            }
+        });
+    }
+
+    end() {
+        return new Promise((resolve, reject) => {
+            this.pool.end(err => {
+                if (err) {
+                    if (this.throwErrors) {
+                        reject(new DatabaseError(err));
+                    } else {
+                        resolve();
+                    }
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    query(...args) {
+        return new Promise((resolve, reject) => {
+            let query;
+
+            const callback = err => {
+                if (err) {
+                    if (this.throwErrors) {
+                        reject(new DatabaseError(err));
+                    } else {
+                        resolve();
+                    }
+                }
+
+                resolve(query);
+            };
+
+            args.push(callback);
+            query = this.pool.query.apply(this.pool, args);
+        });
+    }
+
+    escape(value) {
+        return this.pool.escape(value);
+    }
+
+    escapeId(value) {
+        return this.pool.escapeId(value);
+    }
 }
 
 export default MysqlPool;
