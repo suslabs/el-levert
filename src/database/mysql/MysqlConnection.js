93377ebe973e45f34e43a7557c5e21761c47f6dd
Alex31TheDev <alexdraghia45@gmail.com>
2024-04-02 21:48:53 +0300

    Wrapper 3

--- src/database/mysql/MysqlConnection.js
+++ src/database/mysql/MysqlConnection.js
@@ -1,7 +1,21 @@
+import mysql from "mysql";
+
 class MysqlConnection {
-    constructor(connection, throwErrors) {
-        this.con = connection;
-        this.throwErrors = throwErrors;
+    constructor(config = null, connection) {
+        if (config === null) {
+            this.con = connection;
+            this.config = connection.config;
+            this.throwErrors = this.config.throwErrors;
+        } else {
+            this.config = config;
+            this.connection = mysql.createConnection(config);
+
+            if (typeof config.throwErrors === "boolean") {
+                this.throwErrors = onfig.throwErrors;
+            } else {
+                this.throwErrors = true;
+            }
+        }
     }
 }
 
