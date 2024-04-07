814a93fc4280f8a3eea00e75f336beb304a9b8de
Alex31TheDev <alexdraghia45@gmail.com>
2024-04-08 00:00:06 +0300

    Add transaction support

--- src/database/mysql/MysqlUtil.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql";
-
-const MysqlUtil = {
-    createQuery: (sql, values, callback) => {
-        return mysql.Connection.createQuery(sql, values, callback);
-    }
-};
-
-export default MysqlUtil;
