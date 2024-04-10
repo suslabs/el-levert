c224fe2912d2af7a70fb22c845da25f087555bfc
Alex31TheDev <alexdraghia45@gmail.com>
2024-04-10 23:00:42 +0300

    statement 2

--- src/database/mysql/MysqlStatement.js
+++ src/database/mysql/MysqlStatement.js
@@ -7,6 +7,43 @@ class MysqlStatement {
 
         this.finalize = _ => {};
     }
+
+    bind(...param) {
+        this.defaultParam = param;
+        return this;
+    }
+
+    reset() {
+        return this;
+    }
+
+    run(sql, ...param) {}
+
+    get(sql, ...param) {}
+
+    all(sql, ...param) {}
+
+    each(sql, param, callback) {}
+
+    async getConnection() {
+        if (typeof this.con !== "undefined") {
+            this.releaseConnection();
+        }
+
+        const con = await this.db.getConnection(false);
+        con.throwErrors = true;
+
+        this.con = con;
+    }
+
+    releaseConnection() {
+        if (typeof this.con === "undefined") {
+            return;
+        }
+
+        this.db.releaseConnection(this.con);
+        delete this.con;
+    }
 }
 
 export default MysqlStatement;
