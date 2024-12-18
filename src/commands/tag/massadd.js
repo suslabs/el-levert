62bfa8aac442bce46cf18ebbc667bbbc764fc2d6
Alex31TheDev <alexdraghia45@gmail.com>
2024-12-18 23:57:36 +0200

    Add tag massadd command

---
+++ src/commands/tag/massadd.js
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+
+import { getClient } from "../../LevertClient.js";
+import Util from "../../util/Util.js";
+
+export default {
+    name: "massadd",
+    parent: "tag",
+    subcommand: true,
+    ownerOnly: true,
+
+    handler: async args => {
+        if (args.length === 0) {
+            return "%t massadd tagPrefix owner inputDir";
+        }
+
+        let [tagPrefix, split1] = Util.splitArgs(args),
+            [owner, inputDir] = Util.splitArgs(split1);
+
+        inputDir = path.resolve(inputDir);
+        const files = fs.readdirSync(inputDir);
+
+        files.sort((a, b) =>
+            a.localeCompare(b, "en", {
+                numeric: true
+            })
+        );
+
+        const out = [];
+
+        for (let i = 0; i < files.length; i++) {
+            const filePath = path.join(inputDir, files[i]),
+                contents = fs.readFileSync(filePath, "utf8");
+
+            const tagName = tagPrefix + (i + 1).toString();
+
+            await getClient().tagManager.add(tagName, contents, owner);
+            out.push(`Added file ${files[i]} as tag: ${tagName}`);
+        }
+
+        return out.join("\n");
+    }
+};
